39622cd61038da51f408516cf9ffebb78fbed870
Samriddh Jain <samriddhjn@gmail.com>
2022-01-16 01:48:34 +0530

    frontend functioning

--- Server/routes/getcode.js
+++ Server/routes/getcode.js
@@ -8,7 +8,7 @@ router.get('/api/v1/:getCode', async(req, res) => {
         const doc = await Doc.findOne({ customUrl: getCode }).exec();
 
         if(!doc){
-            return res.status(401).json({ error: 'No document found'});
+            return res.status(404).json({ error: 'No document found'});
         }
 
         return res.status(200).json(doc);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,124 @@
+import React, { useEffect, useRef, useState } from 'react';
+import axios from "axios";
+import { render } from "react-dom";
+import
+{
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import { useSearchParams } from 'react-router-dom';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faPlus, faClock } from "@fortawesome/free-solid-svg-icons";
-import React, { useEffect, useRef } from 'react';
+
 import './App.scss';
+
+interface Bin
+{
+  language: string,
+  customUrl: string,
+  code: string,
+  expiryDate: string,
+}
+
+async function saveBin ( bin: Bin )
+{
+  const response = await axios.post( "http://localhost:4000/api/v1/takeCode", bin );
+  console.log( response );
+}
+
+async function getBin ( binUrl: String ) 
+{
+  const response = await axios.get( `http://localhost:4000/api/v1${ binUrl }` );
+  return response.data;
+}
 
 function App ()
 {
-  const [ code, setCode ] = React.useState(
-    `function add(a, b) {\n  return a + b;\n}`
-  );
+  const [ customUrl, setCustomURL ] = useState( "" );
+  const [ binData, setBinData ] = useState( "" );
+
+  function displayBin ( bin: Bin )
+  {
+    setCustomURL( bin.customUrl );
+    setBinData( bin.code );
+  }
+
+  function newBin() {
+    setCustomURL( "" );
+    setBinData( "" );
+  }
+
+  useEffect( () =>
+  {
+    const binUrl = window.location.pathname;
+
+    async function checkBinUrl () 
+    {
+      if ( binUrl != '/' )
+      {
+        let bin: Bin = await getBin( binUrl );
+        displayBin( bin );
+      }
+
+      else
+      {
+        return null;
+      }
+    };
+
+    checkBinUrl();
+  }, [] );
 
   return (
     <div className="App">
       <section className="header">
         <span className="site-name">spider.bin</span>
-        <input className="custom-url" type="text" placeholder="Custom URL" />
+        <input
+          className="custom-url"
+          type="text"
+          placeholder="Custom URL"
+          value={ customUrl }
+          onChange={ ( event ) =>
+          {
+            setCustomURL( event.target.value );
+          } } />
+
         <div className="btn-wrapper">
-          <FontAwesomeIcon icon={ faSave } className="btn" />
-          <FontAwesomeIcon icon={ faPlus } className="btn" />
+          <FontAwesomeIcon
+            icon={ faSave }
+            className="btn"
+            onClick={ () =>
+            {
+              saveBin( {
+                language: "txt",
+                customUrl: customUrl,
+                code: binData,
+                expiryDate: "fuck you",
+              } );
+            } } />
+          <FontAwesomeIcon
+            icon={ faPlus }
+            className="btn"
+            onClick={ () =>
+            {
+              newBin();
+            }}
+          />
           <FontAwesomeIcon icon={ faClock } className="btn" />
         </div>
       </section>
       <section className="main">
-        <textarea className="editor"></textarea>
+        <textarea
+          className="editor"
+          value={ binData }
+          onChange={ ( event ) =>
+          {
+            setBinData( event.target.value );
+          } }
+        />
       </section>
       <section className="footer">
         <div>
@@ -33,6 +130,7 @@ function App ()
       </section>
     </div>
   );
+
 }
 
 export default App;
