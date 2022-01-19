ad910338d6b4f350c57e14d8b47e85fb49fbd7b2
Samriddh Jain <samriddhjn@gmail.com>
2022-01-19 20:07:07 +0530

    added custom config file for env vars

--- frontend/src/logic/getBin.tsx
+++ frontend/src/logic/getBin.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+import config from '../config.json';
 
 export async function getBin ( binUrl: String )
 {
-  const response = await axios.get( `http://localhost:4000/api/v1${ binUrl }` );
+  const response = await axios.get( `${ config.homeUrl }/api/v1${ binUrl }` );
   return response.data;
 }
--- frontend/src/logic/saveBin.tsx
+++ frontend/src/logic/saveBin.tsx
@@ -1,11 +1,12 @@
 import axios from "axios";
+import config from '../config.json';
 import { Bin } from "./constants";
 
 export async function saveBin ( bin: Bin )
 {
   try
   {
-    const response = await axios.post( "http://localhost:4000/api/v1/takeCode", bin );
+    const response = await axios.post( `${ config.homeUrl }/api/v1/takeCode`, bin );
     console.log( response );
     return response.status;
   }
